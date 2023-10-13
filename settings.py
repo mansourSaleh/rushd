7e534168554b492a29df0ca121d49283e1c5505f
Mansour Almohsen <mansouralmohsen2018@gmail.com>
2023-10-13 05:27:23 +0300

    finish 101

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from os import environ
 SESSION_CONFIGS = [
     dict(
         name='survey',
-        display_name="بنك التنمية الاجماعي",
+        display_name="بنك التنمية الاجتماعية",
         app_sequence=['survey'],
         num_demo_participants=3,
     ),
--- survey/__init__.py
+++ survey/__init__.py
@@ -1,10 +1,12 @@
 from otree.api import *
 
 
-class C(BaseConstants):
-    NAME_IN_URL = 'survey'
-    PLAYERS_PER_GROUP = None
-    NUM_ROUNDS = 1
+class Constants(BaseConstants):
+    name_in_url = 'survey'
+    players_per_group = None
+    num_rounds = 1
+    
+   
 
 
 class Subsession(BaseSubsession):
@@ -15,47 +17,116 @@ class Group(BaseGroup):
     pass
 
 
-class Player(BasePlayer):
-    income = models.IntegerField(
+class Player(BasePlayer):    
+    agree_for_experiment = models.BooleanField(
+        label='''
+            
+        ''',
+        widget=widgets.RadioSelectHorizontal,
+        choices=[
+            [True, 'نعم'],
+            [False, 'لا']
+        ]
+    )
+    monthlyIncome = models.IntegerField(
         label='''
         كم دخلك الشهري
-        '''
+        ''',
+        min=0,  # Set the minimum value (change as needed)
+        max=50000,  # Set the maximum value (change as needed)
+        widget=widgets.TextInput,
     )
-    track_income = models.BooleanField(
+    traceMyIncomeAndExpenses = models.BooleanField(
         label='''
             أقوم بمتابعة وتسجيل دخلي ومصروفاتي
-            '''
+            ''',
+        widget=widgets.CheckboxInput,
+        initial=False,  # Set the default value to False
+        blank=True, 
     )
-    pay_bill = models.BooleanField(
+    canPayMyBillsEasily = models.BooleanField(
         label='''
             أستطيع دفع فواتيري ومصاريفي الشهرية بسهولة
-            '''
+            ''',
+        widget=widgets.CheckboxInput,
+        initial=False,  # Set the default value to False
+        blank=True, 
     )
-    have_goal = models.BooleanField(
+    alreadyHaveFinancialGoals = models.BooleanField(
         label='''
            لدي أهداف مالية أطمح لتحقيقها
-            '''
+            ''',
+        widget=widgets.CheckboxInput,
+        initial=False,  # Set the default value to False
+        blank=True, 
     )
-    total_main_expenses = models.IntegerField(
+    monthlyEssentialExpensesSum = models.IntegerField(
         label='''
         مجموع مصروفاتك الأساسية مثل )السكن، الأكل التعليم...(
-        '''
+        ''',
+        min=0,  # Set the minimum value (change as needed)
+        max=50000,  # Set the maximum value (change as needed)
+        widget=widgets.TextInput,
+        
     )
-    total_not_main_expenses = models.IntegerField(
+    monthlyLuxuryExpensesSum = models.IntegerField(
         label='''
         مجموع مصروفاتك الثانوية مثل )الترفيه، السفر، المطاعم ....(
-        '''
+        ''',
+        min=0,  # Set the minimum value (change as needed)
+        max=50000,  # Set the maximum value (change as needed)
+        widget=widgets.TextInput,
     )
-    debt_monthly = models.IntegerField(
+    monthlyDebtExpenses = models.IntegerField(
         label='''
         مجموع مصروفاتك الثانوية مثل )الترفيه، السفر، المطاعم ....(
-        '''
+        ''',
+        min=0,  # Set the minimum value (change as needed)
+        max=50000,  # Set the maximum value (change as needed)
+        widget=widgets.TextInput,
     )
-    saving_monthly = models.IntegerField(
+    monthlySaving = models.IntegerField(
         label='''
         كم من دخلك يذهب لسداد الديون كم ادخارك الشهري ؟
-        '''
+        ''',
+        min=0,  # Set the minimum value (change as needed)
+        max=50000,  # Set the maximum value (change as needed)
+        widget=widgets.TextInput,
     )
+    age = models.IntegerField(
+        label='''
+        كم عمرك ؟
+        ''',
+        min=0,  # Set the minimum value (change as needed)
+        max=100,  # Set the maximum value (change as needed)
+        widget=widgets.TextInput,
+    )
+    sex = models.StringField(
+        choices=["ذكر", "انثى"],
+        label="النوع",
+        widget=widgets.RadioSelect,
+    )
+    marital_status = models.StringField(
+        choices=["متزوج/رب أسرة", "مطلق", "أرمل", "أعزب"],
+        label="الحالة الاجتماعية",
+        widget=widgets.RadioSelect,
+    )
+    real_estate = models.StringField(
+        choices=["نعم", "لا"],
+        label="هل تملك عقارات؟",
+        widget=widgets.RadioSelect,
+    )
+    have_freelance_certificate = models.StringField(
+        choices=["نعم", "لا"],
+        label="هل لديك شهادة عمل حر ؟",
+        widget=widgets.RadioSelect,
+    )
+    have_productive_family = models.StringField(
+        choices=["نعم", "لا"],
+        label="هل لديك شهادة الأسر المنتجة؟",
+        widget=widgets.RadioSelect,
+    )
+        
 
 
 # FUNCTIONS
@@ -63,13 +134,146 @@ class Player(BasePlayer):
 
 class Intro(Page):
     form_model = 'player'
-    form_fields = ['income', 'track_income', 'pay_bill',
-                   'have_goal',
-                   'total_main_expenses',
-                   'total_not_main_expenses', 'debt_monthly',
-                   'saving_monthly' ]
+    form_fields = ['agree_for_experiment']
+    
+    @staticmethod
+    def is_displayed(player: Player):
+        return True
+    
+    @staticmethod
+    def vars_for_template(player: Player):
+       return {
+            'field_name': 'agree_for_experiment',
+       }
+    
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        print('values isssss')
+        if not player.agree_for_experiment:
+            player.participant.vars['dropout_fields'] = ['agree_for_experiment']
+            player.participant.vars['dropout_page'] = 'Intro'
+        
+    
+    # @staticmethod
+    def error_message(player: Player, values):
+        # print the values;
+        print('values is', values)
+        if not values['agree_for_experiment']:
+            return 'يجب الموافقة على المشاركة في الدراسة للمتابعة'
+        
+    
+            
+
+
+class FinancialAnalysis(Page):
+    form_model = 'player'
+    form_fields = [ 'monthlyIncome', 'traceMyIncomeAndExpenses',
+                   'canPayMyBillsEasily', 'alreadyHaveFinancialGoals',
+                   'monthlyEssentialExpensesSum', 'monthlyLuxuryExpensesSum','monthlyDebtExpenses',  'monthlySaving'            
+    ]
+    
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        print('values isssss')
+        # if not player.agree_for_experiment:
+        #     player.participant.vars['dropout_fields'] = ['agree_for_experiment']
+        #     player.participant.vars['dropout_page'] = 'Intro'
+        
+    
+    @staticmethod
+    def error_message(self, values):
+        print('values is', values)
+        if not values['monthlyIncome']:
+            return 'Custom error message for my_field'
+    
+    
+    @staticmethod
+    def vars_for_template(player: Player):
+        return{
+            'step': 1,  # Adjust the step value as needed
+            'monthlyIncome': 'monthlyIncome'
+        }
 
 
 
+class PersonalAnalysis(Page):
+    form_model = 'player'
+    form_fields = ['age', 'sex', 'marital_status', 'real_estate']
+    
+    @staticmethod
+    def is_displayed(player: Player):
+        return True
+    
+    @staticmethod
+    def vars_for_template(player: Player):
+       return {
+            'step': 1,  # Adjust the step value as needed
+       }
+       
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        print('values isssss')
+        if not player.age:
+            player.participant.vars['dropout_fields'] = ['age']
+            player.participant.vars['dropout_page'] = 'PersonalAnalysis'
+            
+    @staticmethod
+    def error_message(player: Player, values):
+        # print the values;
+        print('values is', values)
+        if not values['age']:
+            return 'يجب تعبئة الحقل'
+        
+ 
+    
+class Freelance(Page):
+    form_model = 'player'
+    form_fields = ['have_freelance_certificate']
+    
+    @staticmethod
+    def is_displayed(player: Player):
+        return True
+    
+   
+       
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        pass
+            
+    @staticmethod
+    def error_message(player: Player, values):
+        # print the values;
+        print('values is', values)
+        if not values['have_freelance_certificate']:
+            return 'يجب تعبئة الحقل'
+        
+    
+    
+class Productive_Family(Page):
+    form_model = 'player'
+    form_fields = ['have_productive_family']
+    
+    @staticmethod
+    def is_displayed(player: Player):
+        return True
+    
+   
+       
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        pass
+            
+    @staticmethod
+    def error_message(player: Player, values):
+        # print the values;
+        print('values is', values)
+        if not values['have_productive_family']:
+            return 'يجب تعبئة الحقل'
+        
+    
 
-page_sequence = [Intro]
+class Result(Page):
+    pass
+
+
+page_sequence = [Intro, FinancialAnalysis,  PersonalAnalysis ,Freelance,  Productive_Family, Result, ]
